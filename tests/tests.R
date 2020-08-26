4091aeb16f6874bafde719061436f33c8c7b6b3b
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2020-08-26 16:46:25 -0700

    First pass of 60 or so test cases, TMB model not matching anymore

--- tests/tests.R
+++ tests/tests.R
@@ -14,11 +14,52 @@ test <- file.copy('rem.exe', to='../tests/rem_runs/rem.exe', overwrite=TRUE)
 if(!test) stop("Failed to make new rem.exe model")
 setwd('../tests')
 
-## RE model tests
-file.copy('data/AI_Kamchatka.dat', to='re_runs/AI_Kamchatka.dat', overwrite=TRUE)
-setwd('re_runs')
-system('re -ind AI_Kamchatka.dat')
-out <- read_re_output('rwout.rep')
-setwd('..')
+library(TMB)
+dyn.unload(dynlib('../src/re_tmb'))
+compile('../src/re_tmb.cpp')
+dyn.load(dynlib('../src/re_tmb'))
 
-expect_known_output(out, file='testthat/_expect_AI_Kamchatka')
+
+## RE model tests
+
+test_re_species <- function(d){
+  m <- gsub('.dat','',d)
+  file.copy(file.path('data', d), to=file.path('re_runs',d), overwrite=TRUE)
+  setwd('re_runs')
+  on.exit(setwd('..'))
+  message("Testing RE model for ", m)
+  test <- system(paste('re -ind', d), ignore.stdout=TRUE)
+  ## if(test!=0) stop('Failed to run ', m)
+  out <- read_re_output('rwout.rep')
+  expect_known_output(out, file=paste0('../testthat/_expect_', m))
+  ## Get the data for TMB from the RE output
+  srv_sd <- out$srv_sd[!is.na(out$srv_sd)]
+  srv_est <- out$srv_est[!is.na(out$srv_est)]
+  ## srv_cv  <- sqrt(log(1+(srv_se/srv_est)^2))
+  yrs_srv <- out$year[!is.na(out$srv_est)]
+  data <- list(yrs=out$year, yrs_srv=yrs_srv,
+               yrs_srv_ind=match(yrs_srv, out$year)-1,
+               srv_est=srv_est, srv_sd=srv_sd)
+  pars <- list(logSdLam=1, biom=out$log_biomass)
+  obj <- MakeADFun(data, pars, random='biom')
+  obj$env$beSilent()
+  opt <- with(obj, nlminb(par, fn, gr))
+  adrep <- sdreport(obj)
+  results <- cbind(species=m, out, tmb_log_biomass=adrep$value, tmb_SE_log=adrep$sd)
+  return(results)
+}
+
+spp <- list.files('data', pattern='.dat')
+results <- lapply(spp, test_re_species) %>% do.call(rbind,.)
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+results <- results %>%
+  mutate(log_biomass_relerror=(log_biomass-tmb_log_biomass)/log_biomass,
+         SE_log_relerror=(SE_log-tmb_SE_log)/SE_log) %>%
+  pivot_longer(cols=c('SE_log_relerror', 'log_biomass_relerror'),
+  values_to='relative_error')
+
+ggplot(results, aes(year, relative_error, color=name)) + geom_point()
